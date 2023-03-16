f1cd6bcde993209be6f6b04461908c15eeaaf7ea
Chitoku <chitoku-k@users.noreply.github.com>
2023-03-16 22:36:13 +0900

    Remove use of the deprecated `TestFixture::$fields` (#41)

--- tests/Fixture/PostsFixture.php
+++ tests/Fixture/PostsFixture.php
@@ -8,17 +8,6 @@ use Cake\TestSuite\Fixture\TestFixture;
 
 class PostsFixture extends TestFixture
 {
-    public $fields = [
-        'id' => ['type' => 'integer'],
-        'modified' => ['type' => 'datetime'],
-        '_constraints' => [
-            'primary' => [
-                'type' => 'primary',
-                'columns' => ['id'],
-            ],
-        ],
-    ];
-
     public $records = [
         ['id' => 1, 'modified' => '2017-01-01 10:00:00'],
         ['id' => 3, 'modified' => '2017-01-01 10:00:00'],

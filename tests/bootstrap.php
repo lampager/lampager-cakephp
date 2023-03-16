fe42780be9be2a5d59739f09d80e6b9a0938594c
Chitoku <chitoku-k@users.noreply.github.com>
2023-03-16 23:09:43 +0900

    Remove unused `Lampager\Cake\TestApp` (#42)
    * Remove unused `Lampager\Cake\TestApp`
    
    * Remove unused configurations for testing
    
    * Test with sqlite:///:memory: with `DB_DSN` empty

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Cake\Cache\Cache;
-use Cake\Core\Configure;
 use Cake\Database\Connection;
 use Cake\Datasource\ConnectionManager;
 use Cake\TestSuite\Fixture\SchemaLoader;
@@ -12,35 +10,8 @@ use Lampager\Cake\Database\Driver\Sqlite;
 require_once __DIR__ . '/../vendor/cakephp/cakephp/src/basics.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('ROOT', dirname(__DIR__));
-define('CORE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
-define('APP', ROOT . DS . 'tests' . DS . 'test_app' . DS);
-define('CONFIG', APP);
-define('TMP', sys_get_temp_dir() . DS);
-define('CACHE', TMP . 'cache' . DS);
-
-Configure::write('debug', true);
-Configure::write('App', [
-    'debug' => true,
-    'namespace' => 'App',
-    'encoding' => 'UTF-8',
-]);
-
-Cache::setConfig([
-    '_cake_core_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_core_',
-        'serialize' => true,
-        'path' => CACHE,
-    ],
-]);
-
-if (!getenv('DB_DSN')) {
-    putenv('DB_DSN=sqlite:///:memory:?className=' . Connection::class . '&driver=' . Sqlite::class . '&quoteIdentifiers=true');
-}
-
 ConnectionManager::setConfig('test', [
-    'url' => getenv('DB_DSN'),
+    'url' => env('DB_DSN') ?: 'sqlite:///:memory:?className=' . Connection::class . '&driver=' . Sqlite::class . '&quoteIdentifiers=true',
 ]);
 
 // Create test database schema
